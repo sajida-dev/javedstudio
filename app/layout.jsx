00577357a7b97b9ae7d894aca2eb514a989b02f1
sajida-dev <sajidajaved604@gmail.com>
2025-02-15 15:21:13 +0500

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/nav";
+import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
 const geistSans = Geist({
