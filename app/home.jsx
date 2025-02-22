d377156a86ac45abbd5653f7558d902c6f466f85
sajida-dev <sajidajaved604@gmail.com>
2025-02-23 00:59:40 +0500

    commit

--- app/home.jsx
+++ app/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <SplitText text="PRODUCT PHOTOGRAPHY" />
+      <SplitText text="WEDDING PHOTOGRAPHY" />
 
       <ImageGallery />
     </>
