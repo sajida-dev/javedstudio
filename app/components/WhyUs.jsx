eda51e4473eb008ac70a07289470486fc650991d
sajida-dev <sajidajaved604@gmail.com>
2025-03-30 00:02:12 +0500

    commit

--- app/components/WhyUs.jsx
+++ app/components/WhyUs.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 const WhyUs = () => {
     return (
         <section className="text-gray-700 body-font">
-            <div className="flex justify-center mt-10 text-4xl font-regular">
+            <div className="flex justify-center my-10 text-4xl font-regular">
                 Why Us?
+            </div>
+            <div className="relative mx-auto max-w-5xl text-center">
+                <h2 className="block w-full bg-gradient-to-b from-white to-gray-400 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
+                    Build a Website That Your Customers Love
+                </h2>
+                <p className="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-400">
+                    Our templates allow for maximum customization. No technical skills required – our intuitive design tools
+                    let you get the job done easily.
+                </p>
             </div>
             <div className="container px-5 py-12 mx-auto">
                 <div className="flex flex-wrap text-center justify-center">
