e8d669dc8eb268a3edee399a7ca7769e8c8ce580
sajida-dev <sajidajaved604@gmail.com>
2025-04-06 16:09:46 +0500

    final commit

--- app/service/baby/GalleryData.jsx
+++ app/service/baby/GalleryData.jsx
@@ -1,10 +1,15 @@
 // image id's of cloudinary images
 
 export const babyGallery = [
+    "DSC_1688_bdmqs0",
+    "DSC_5119_copy_rilgnx",
+    "DSC_0726_gk1tda",
+    "AKS_0275_irq76c",
+    "AKS_0273_bcbyv5",
+    "AKS_0276_ugy6m1",
+    "AKS_0277_atnaqh",
     "13_nkpanv",
     "7_jgnuvp",
-    "3_zleexg",
-    "3_zleexg",
     "3_zleexg",
     "14_ondovd",
     "15_einwxh",
@@ -12,7 +17,6 @@ export const babyGallery = [
     "1_drtc3w",
     "8_momhsf",
     "5_f06stw",
-    "11_fh09ol",
     "11_fh09ol",
     "6_mmkdiy",
     "2_flkvvz",
